1eb692dc1261b261df885f11441c20820a6c20d2
Dong Linyun <easterdong@maycur.com>
2016-04-15 11:38:52 +0800

    Android external browser.

--- src/android/Browser.java
+++ src/android/Browser.java
@@ -1,21 +1,26 @@
 package com.maycur.plugin;
 
+import android.content.Intent;
+import android.net.Uri;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 public class Browser extends CordovaPlugin {
-
-    private PickerNode[] mNodes;
-    CallbackContext mCallbackContext;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("show")) {
-            
-            return true;
+            String url = args.getString(0);
+            if (url != null && !url.isEmpty()) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                cordova.getActivity().startActivity(intent);
+                return true;
+            }
+
+            return false;
         }
         return false;
     }
